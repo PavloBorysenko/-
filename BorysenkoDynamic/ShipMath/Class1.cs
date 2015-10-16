4d76f04550a05eea8351fa9fbd88ab0140915f64
PavloBorysenko <irod-09@mail>
2015-10-16 22:04:47 +0200

    Отработка DLL

---
+++ BorysenkoDynamic/ShipMath/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShipMath
+{
+    public class Functions
+    {
+        public int Add(int m, int w)
+        {
+            return w + m;
+
+
+        }
+    }
+}
